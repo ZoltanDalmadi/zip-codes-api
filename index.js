c97a50fa26d5b15cec4825f1d8a4d128a9965bbe
Zoltan Dalmadi <dmz985@gmail.com>
2018-03-17 14:27:54 +0100

    Use util.promisify instead of manual Promise wrapping

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 const { send } = require("micro");
 const { router, get } = require("microrouter");
+const util = require('util');
 
 const sqlite3 = require("sqlite3");
 const db = new sqlite3.Database("zips.db");
 
-// wraps sqlite call into a Promise
-const db_all = (db, command, ...params) =>
-  new Promise((resolve, reject) =>
-    db.all(command, params, (err, rows) =>
-      err ? reject(err) : resolve(rows)));
+const db_all = util.promisify(db.all.bind(db));
 
-const getCountries = async () => await db_all(db,
+const getCountries = async () => await db_all(
   "SELECT * FROM countries");
 
 const getCities = async req => {
-  const cities = await db_all(db,
+  const cities = await db_all(
     `SELECT cities.id, cities.name FROM cities
     JOIN countries ON cities.country_id=countries.id
     WHERE countries.name=?`, req.params.name);
@@ -22,11 +19,11 @@ const getCities = async req => {
   // sub-query to get all zip codes for each city
   return Promise.all(
     cities.map(async city => ({ ...city,
-      zips: (await db_all(db, "SELECT name FROM zips where city_id=?", city.id)).map(zip => zip.name)
+      zips: (await db_all("SELECT name FROM zips where city_id=?", city.id)).map(zip => zip.name)
     })));
 };
 
-const getZips = async req => await db_all(db,
+const getZips = async req => await db_all(
   `SELECT name as zip FROM zips
   WHERE city_id=?`, req.params.id);
 
